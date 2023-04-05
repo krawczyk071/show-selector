c9e326a68d9c57a41164ea381c2be4a1e93ff2d2
krawczyk071 <krawczyk071@gmail.com>
2023-04-05 17:45:26 +0200

    init clean

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      This is app
+    </div>
+  );
+}
+
+export default App;
